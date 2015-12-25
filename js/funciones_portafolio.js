35c878c2bca972f1b6ce3d7f089fd4bb2dda68c5
autvincere <aut.vincere@gmail.com>
2015-12-25 13:44:39 -0300

    slider js incluido

--- js/funciones_portafolio.js
+++ js/funciones_portafolio.js
@@ -6,17 +6,31 @@ $(document).ready(function(){
 			var desdeTop = $(window).scrollTop();
 	//427
 		if(desdeTop >42){
-			console.log('es mayor :)')
-			$('div.container').addClass('down_container');
-            $('div.container nav').addClass('down_nav');
-            $('div.container a figure img').addClass('down_logo');
+			//console.log('es mayor :)')
+			$('div.content').addClass('down_content');
+            $('div.content nav').addClass('down_nav');
+            $('div.content a figure img').addClass('down_logo');
 		}else{
-			$('div.container').removeClass('down_container');
-            $('div.container nav').removeClass('down_nav');
-            $('div.container a figure img').removeClass('down_logo');                               
+			$('div.content').removeClass('down_content');
+            $('div.content nav').removeClass('down_nav');
+            $('div.content a figure img').removeClass('down_logo');                               
 
 		}
 		});
+    //SLIDER JS
+    $(function() {
+      $('#slides').slidesjs({
+        width: 940,
+        height: 528,
+        play: {
+          active: true,
+          auto: true,
+          interval: 4000,
+          swap: true
+        }
+      });
+    });
+    //FIN SLIDER JS
     
 
 });
